25927c4627c0317a84b61e7ead627b8aa553ef9b
Jeremy Sears <jeremysears@users.noreply.github.com>
2015-10-10 16:13:31 -0400

    Simple "hello world" in Scala.

---
+++ src/fpinscala/gettingstarted/main.scala
@@ -0,0 +1,9 @@
+package fpinscala.gettingstarted
+
+/**
+ * Simple Hello-world
+ * Created by jsears on 10/10/15.
+ */
+object main {
+  def main(args: Array[String]): Unit = println("hello world")
+}
